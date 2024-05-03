95e8cf60e7aed97ba318acdfdab1f2d550e8b405
Delemangi <milev.stefan@gmail.com>
2024-05-03 14:50:02 +0200

    Update translations

--- bot/src/translations/errors.ts
+++ bot/src/translations/errors.ts
@@ -3,12 +3,11 @@ export const errors = {
   commandError: 'There was an error while executing the command.',
   commandExecutionFailed: (commandName: string) =>
     `Command ${commandName} execution failed.`,
-  commandNoPermission: 'You do not have permission to execute this command.',
   noApplicationId:
     'No application ID provided. Please provide an application ID in the .env file.',
   noStrapiUrl:
     'No Strapi URL provided. Please provide a Strapi URL in the .env file.',
   noToken: 'No token provided. Please provide a token in the .env file.',
-  questionNotDefinedCorrectly: 'Question not defined correctly.',
-  questionNotFound: 'Question not found.',
+  questionNotDefinedCorrectly: 'The question is not defined correctly.',
+  questionNotFound: 'That question does not exist.',
 };
