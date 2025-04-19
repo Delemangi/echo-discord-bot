d65cfc11f2bbc83c06e10c9c7fbead0b7b908105
Delemangi <milev.stefan@gmail.com>
2025-04-19 20:15:29 +0200

    Refactor hot reloading

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -1,8 +1,11 @@
+import { config } from 'dotenv';
+
 import { getBotCredentials } from './config/environment.js';
 import { client } from './utils/client.js';
 import { registerCommands } from './utils/commands.js';
 import { attachEventListeners } from './utils/events.js';
-import 'dotenv/config';
+
+config();
 
 // Initialization
 
